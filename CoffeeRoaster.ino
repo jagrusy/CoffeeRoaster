b4bf640d49e7839793e47d7a9e26f2f25fb622f5
nhall34 <nhall34@gatech.edu>
2016-11-04 13:05:29 -0400

    Update CoffeeRoaster.ino

--- CoffeeRoaster.ino
+++ CoffeeRoaster.ino
@@ -1,38 +1,81 @@
-#define RELAY1 2
-#define RELAY2 4
+const int sensorPin = A0;
+const float maxtemp = 450.0;
+const float mintemp = 420.0;
+#define relay1 2
+#define relay2 4
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#define TFT_CS     10
+#define TFT_RST    9
+#define TFT_DC     8
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
-unsigned long startTime = millis();
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
 
-void setup() {
-  // Set up pin outputs for relays
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
+void setup(void) {
+  Serial.begin(9600);
 
-  // Default relay to off
-  digitalWrite(RELAY1, HIGH);
-  digitalWrite(RELAY2, HIGH);
-  // Initialize thermocouples
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
 
-  // Call menu
-  
+  uint16_t time = millis();
+  tft.fillScreen(ST7735_BLACK);
+  time = millis() - time;
 
+  Serial.println(time, DEC);
+  delay(1000);
+
+  // large block of text
+  tft.fillScreen(ST7735_BLACK);
+  testdrawtext("Welcome Home, Baby", ST7735_WHITE);
+  delay(3000);
+
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  delay(2000);
 }
 
 void loop() {
-    delay(10000);
-    // LOW turns coils ON
-    digitalWrite(RELAY1, LOW);
-    digitalWrite(RELAY2, LOW);
-}
+   tft.invertDisplay(false);
+   int A0 = analogRead(0);
+   // equation to calculated temperature
+   double Vin=(5*A0);
+   float vinl=Vin/1024;
+  double Vout=vinl-1.25;
+  double T = Vout/0.005;
+  double TF = (T*1.8)+32;
 
-int getTemp() {
-  double temp;
+  //print temperature in Celsius
+  Serial.println(TF);
   
-  // read timer
+  if(TF<mintemp){
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+  }
+  else{
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+  }
 
-
-  // calculate target temp
-  
-
+  tft.setTextWrap(false);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextSize(1);
+  tft.setTextColor(ST7735_WHITE);
+  tft.print("Current Temp: ");
+  tft.println(TF);
+  tft.println("Current Time: ");
+  uint16_t time = millis();
+  time = millis() - time;
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST7735_WHITE);
+  tft.print(" seconds");
+  delay(835);
 }
-
